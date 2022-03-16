4c695ea14f33112e5e286470d44995f5bfe4a994
Jiwoo Kim <kimziwoo725@gmail.com>
2022-03-16 16:13:40 +0900

    cubit_app

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,237 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/cubit/app_cubit_states.dart';
+import 'package:flutter_cubit/cubit/app_cubits.dart';
+import 'package:flutter_cubit/misc/colors.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  // List 써도 됨
+  var images = {
+    'balloning.png': 'Balloning',
+    'hiking.png': 'Hiking',
+    'kayaking.png': 'Kayaking',
+    'snorkling.png': 'Snorkling'
+  }; // 앞에가 keys값 : 뒤에가 values값
+
+  @override
+  Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(body: BlocBuilder<AppCubits, CubitStates>(
+      builder: (context, state) {
+        if (state is LoadedState) {
+          var info = state.places;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 70, left: 20),
+                child: Row(
+                  children: [
+                    // menu text
+                    Icon(
+                      Icons.menu,
+                      size: 30,
+                      color: Colors.black54,
+                    ),
+                    Expanded(child: Container()),
+                    Container(
+                      margin: const EdgeInsets.only(right: 20),
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.withOpacity(0.5),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              // discover text
+              Container(
+                margin: const EdgeInsets.only(left: 20),
+                child: AppLargeText(text: 'Discover'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              // tabbar
+              Container(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TabBar(
+                    labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                    controller: _tabController,
+                    labelColor: Colors.black,
+                    unselectedLabelColor: Colors.grey,
+                    isScrollable: true,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    indicator: CircleTabIndicator(
+                      color: AppColors.mainColor,
+                      radius: 4,
+                    ),
+                    tabs: [
+                      Tab(
+                        text: 'Places',
+                      ),
+                      Tab(
+                        text: 'Inspiration',
+                      ),
+                      Tab(
+                        text: 'Emotions',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 20),
+                height: 300,
+                width: double.maxFinite,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    ListView.builder(
+                      itemCount: info.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (BuildContext context, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            BlocProvider.of<AppCubits>(context)
+                                .detailPage(info[index]);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 15, top: 10),
+                            width: 200,
+                            height: 300,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  'http://mark.bslmeiyu.com/uploads/' +
+                                      info[index].img,
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Text('There'),
+                    Text('Bye'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppLargeText(
+                      text: 'Explore more',
+                      size: 22,
+                    ),
+                    AppText(
+                      text: 'See all',
+                      color: AppColors.textColor1,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 120,
+                width: double.maxFinite,
+                margin: const EdgeInsets.only(left: 20),
+                child: ListView.builder(
+                    itemCount: 4,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, index) {
+                      return Container(
+                        margin: const EdgeInsets.only(right: 30),
+                        child: Column(
+                          children: [
+                            Container(
+                              // margin: const EdgeInsets.only(right: 50),
+                              width: 80,
+                              height: 80,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    'img/' + images.keys.elementAt(index),
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              child: AppText(
+                                text: images.values.elementAt(index),
+                                color: AppColors.textColor2,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              )
+            ],
+          );
+        } else {
+          return Container();
+        }
+      },
+    ));
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+  CircleTabIndicator({required this.color, required this.radius});
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePainter(color: color, radius: radius);
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+  _CirclePainter({required this.color, required this.radius});
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+    final Offset circleOffset = Offset(
+        configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius);
+
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
+  }
+}
---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/pages/navpages/bar_item_page.dart';
+import 'package:flutter_cubit/pages/home_page.dart';
+import 'package:flutter_cubit/pages/navpages/my_page.dart';
+import 'package:flutter_cubit/pages/navpages/search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    HomePage(),
+    BarItemPage(),
+    SearchPage(),
+    MyPage(),
+  ];
+
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex, // List pages의 몇 번째를 고정으로 할 건지
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(
+              label: 'Bar', icon: Icon(Icons.bar_chart_sharp)),
+          BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: 'My', icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
